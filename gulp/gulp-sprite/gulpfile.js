8544a3ec9c1d5c2cc2531f12e5a41bea91d0f1f9
LucianoMacTrampo <baraunalucia@gmail.com>
2016-07-26 18:33:12 -0300

    Add clean

--- gulp/gulp-sprite/gulpfile.js
+++ gulp/gulp-sprite/gulpfile.js
@@ -1,5 +1,6 @@
 // Modulos gulp
 var gulp = require('gulp'),
+    gulpclean = require('gulp-clean');
     gulpif = require('gulp-if'),
     sprity = require('sprity');
 
@@ -8,12 +9,22 @@ gulp.task('otimizar', ['copy'], function() {
 	gulp.start('build-img', 'copy-libs',  'build-usemin');
 });
 
-gulp.task("sprite", function(){
+gulp.task('sprite', ['criar'], function(){
     return sprity.src({
                         src: 'src/img/**/*.{png,jpg}',
                         style: 'src/css/estilo.css',
                         cssPath: '../img/',
-                        prefix: 'buceta'
+                        prefix: 'buceta',
                     })
                     .pipe(gulpif('*.png', gulp.dest('./dist/img/'), gulp.dest('./dist/css/')));
+});
+
+gulp.task('criar', ['clean'],function(){
+    return gulp.src(' ')
+               .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean',function(){
+    return gulp.src('dist')
+                .pipe(gulpclean());
 });
